f76b497ddbd05853d866c9f11e0b1ea713f0021a
Munish Sharma <34977055+itsMunish@users.noreply.github.com>
2023-12-09 11:15:23 +0000

    datatype

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,19 @@
+"use strict" //treat all js code as newer version of js
+
+// alert("hello"); //we are using nodejs not browser it is not work here
+
+console.log(3+3);
+let age = 28
+// number
+// bigint
+// string = ""
+// boolean = true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+console.log(typeof"Munish");//6
+console.log(typeof(age));//number
+console.log(typeof(null)); //object   null is a object
+console.log(typeof(undefined)); //undefined
