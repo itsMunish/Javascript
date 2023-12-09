c1665cf378eb9a1b1ff0b9b8f67f93394d14acbb
Munish Sharma <34977055+itsMunish@users.noreply.github.com>
2023-12-09 11:52:08 +0000

    opration done

--- 01_basic/03_conversionOrOperation.js
+++ 01_basic/03_conversionOrOperation.js
@@ -1,4 +1,4 @@
-// type conversion
+// ******************type conversion**************************
 let score = "33abc";
 console.log(typeof score); //string
 console.log(typeof(score)); //string
@@ -24,3 +24,28 @@ console.log(typeof valueInNumber1);  //number
 let isLoggedIn = 1;
 let bolleanIsLoggedIn  = Boolean (isLoggedIn);
 console.log(bolleanIsLoggedIn);
+
+
+//********************************Operations *****************************//
+
+let value = 3
+let negvalue = -value;
+ // console.log(negvalue)  //-3
+
+ let str = "munish";
+ let str1 = " sharma";
+ let str2 = str+ str1;
+ console.log(str2);
+
+ console.log("1" + 2); //12
+ console.log(1+"2");  //12
+ console.log("1"+"2");//12
+ console.log("1"+2 + 2); //122
+ console.log(1+2 + "2");   //32
+
+ let gameCounter = 100
+ gameCounter++; 
+ console.log(gameCounter);
+
+ // perfix and postfix https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+ 
