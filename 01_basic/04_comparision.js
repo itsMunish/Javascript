7924921685eb369ac625fd9fabe3e634e9848744
Munish Sharma <34977055+itsMunish@users.noreply.github.com>
2023-12-09 12:02:32 +0000

    comparision done

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,11 @@
+//comparision always check same data type while compamaring
+ // === stricket check
+
+ 
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+console.log(undefined == 0); //false
+console.log("2" === 2);
+
+//avoid these type of code.
