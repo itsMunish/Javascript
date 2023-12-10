df708b3c19f10a64c3d2febb0f5bc3f3582ab6dd
Munish Sharma <34977055+itsMunish@users.noreply.github.com>
2023-12-10 10:15:31 +0000

    string done

---
+++ 01_basic/05_string.js
@@ -0,0 +1,16 @@
+//String Interpolation
+
+const name = "Munish";
+const repoCount = 50;
+
+console.log(`hello this is my ${name} and my repo count is ${repoCount}`);
+//defining String
+const gameName = new String("helloWorld")
+console.log(gameName[0]);
+
+console.log(gameName.__proto__);
+console.log(gameName.length);
+
+const newgame = gameName.substring(0,4);
+console.log(newgame);
+
