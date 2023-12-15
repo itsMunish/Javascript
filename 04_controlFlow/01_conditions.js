70731c0ff6802b9369cf6824b74fb56a962ce54b
Munish Sharma <34977055+itsMunish@users.noreply.github.com>
2023-12-15 06:32:36 +0000

    switch

---
+++ 04_controlFlow/01_conditions.js
@@ -0,0 +1,16 @@
+const month = 3;
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("march");
+        break;
+
+    default:
+        console.log("No case Match");
+        break;
+}
