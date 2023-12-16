302f2fe9926e4603794954755183a4ec5e26d9f5
Munish Sharma <34977055+itsMunish@users.noreply.github.com>
2023-12-16 14:56:06 +0000

    chaining

---
+++ 05_itration/03_three.js
@@ -0,0 +1,13 @@
+const mapmyNum = [1,2,3,4,5,6,7,8,9,10]
+// const newNums = mapmyNum.map((num) => num+10)
+// console.log(newNums); //[
+                        //  11, 12, 13, 14, 15,
+                            //     16, 17, 18, 19, 20
+                            // ]
+        
+//++++++++++++++++++++++++chaning++++++++++++++++++++++++++++++++
+const newNums = mapmyNum    
+                            .map((num) => num*10) //[10,20,30,40,50,60,70,80,90,100]
+                            .map((num) => num +1) // [11, 21, 31, 41,  51,61, 71, 81, 91, 101]
+                            .filter((num) => num >= 40) //[ 41, 51,  61, 71,81, 91, 101]
+console.log(newNums);
