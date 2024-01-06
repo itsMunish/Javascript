063382e333b980fe41d04c828113abfe38fb96d3
Munish Sharma <munish.sharma@sunjewels.com>
2024-01-06 17:25:17 +0530

    color game

---
+++ Advance/07_project/main.js
@@ -0,0 +1,29 @@
+//genrate a random color 
+const randomColor =function(){
+    const hex = "0123456789ABCDEF";
+    let color = '#'
+    for(let i =0;i<6;i++){
+        color += hex[Math.floor(Math.random()*16)]
+    } 
+    return color;
+}
+let intervalId
+
+const StartChangingColor = function(){
+    if(!intervalId){
+    intervalId= setInterval(changeColor,1000);
+}
+   function changeColor(){
+     document.body.style.backgroundColor = randomColor()
+}
+}
+const StopChangingColor = function(){
+    clearInterval(intervalId)
+    intervalId = null;
+}
+
+document.querySelector("#start")
+.addEventListener('click',StartChangingColor)
+
+document.querySelector("#stop")
+.addEventListener('click',StopChangingColor)
